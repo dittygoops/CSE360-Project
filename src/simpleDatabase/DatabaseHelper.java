e5b919ac4787110db0e3cabd2e2eb75686b8fe81
dittygoops <aditya.gup16@gmail.com>
2024-12-06 18:18:20 -0700

    finishing search i think

--- src/simpleDatabase/DatabaseHelper.java
+++ src/simpleDatabase/DatabaseHelper.java
@@ -1903,52 +1903,36 @@ class DatabaseHelper {
 	}
 
 	public void searchArticle(String role, String level, String group, String search) { 
-        if (role.equals("s")) {
-            System.out.println("Invalid role");
-            return;
-        }
-
-        String sql = "SELECT DISTINCT a.* FROM articles a "
-                + "JOIN articleGroups ag ON a.id = ag.article_id "
-                + "WHERE LOWER(a.level) LIKE ? "
-                + "AND LOWER(ag.group_name) LIKE ? "
-                + "AND (LOWER(a.keywords) LIKE ? "
-                + "OR LOWER(a.title) LIKE ? "
-                + "OR LOWER(a.short_description) LIKE ?)";
-
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            String searchPattern = "%" + search.toLowerCase() + "%";
-            String levelPattern = "%" + level.toLowerCase() + "%";
-            String groupPattern = "%" + group.toLowerCase() + "%";
-            
-            pstmt.setString(1, levelPattern);
-            pstmt.setString(2, groupPattern);
-            pstmt.setString(3, searchPattern);
-            pstmt.setString(4, searchPattern);
-            pstmt.setString(5, searchPattern);
-
-            try (ResultSet rs = pstmt.executeQuery()) {
-                List<Article> articles = new ArrayList<>(); 
-
-                while (rs.next()) {
-                    articles.add(new Article(rs.getInt("id"), rs.getString("level"), rs.getString("group_name"), rs.getString("title"), rs.getString("short_description"), rs.getString("keywords"), rs.getString("body"), rs.getString("reference_links")));
-                }
-
-                System.out.println("Search Level: " + level + "\t\tTotal Results: " + articles.size());
-
-                for (int i = 0; i < articles.size(); i++) {
-                    System.out.println(i+1 + ". " + articles.get(i).toString());
-                }
-
-                System.out.println("Which article would you like to view? (Enter the number)");
-                int choice = Integer.parseInt(scanner.nextLine());
-                // viewArticle(role, String.valueOf(articles.get(choice-1).getId()), false);
-				System.out.println("One article found: 240968");
-            }
-        } catch (SQLException e) {
-            System.err.println("Database error while searching articles: " + e.getMessage());
-        }
-    }
-
-	
+		String query = "SELECT a.* FROM articles a "
+					 + "LEFT JOIN articleGroups ag ON a.id = ag.article_id "
+					 + "WHERE (? = 'ALL' OR a.level = ?) "
+					 + "AND (? = 'ALL' OR ag.group_name LIKE ?) "
+					 + "AND (a.title LIKE ? OR a.short_description LIKE ? OR a.keywords LIKE ?)";
+		try (PreparedStatement pstmt = connection.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+			pstmt.setString(1, level);
+			pstmt.setString(2, level);
+			pstmt.setString(3, group);
+			pstmt.setString(4, group.equals("ALL") ? "%" : "%" + group + "%");
+			pstmt.setString(5, "%" + search + "%");
+			pstmt.setString(6, "%" + search + "%");
+			pstmt.setString(7, "%" + search + "%");
+			try (ResultSet rs = pstmt.executeQuery()) {
+				List<Article> articles = new ArrayList<>();
+				while (rs.next()) {
+					int id = rs.getInt("id");
+					String articleLevel = rs.getString("level");
+					String authors = rs.getString("authors");
+					String title = rs.getString("title");
+					String shortDescription = rs.getString("short_description");
+					String keywords = rs.getString("keywords");
+					String body = rs.getString("body");
+					String referenceLinks = rs.getString("reference_links");
+					articles.add(new Article(id, articleLevel, authors, title, shortDescription, keywords, body, referenceLinks));
+				}
+				// Process the articles list as needed
+			}
+		} catch (SQLException e) {
+			System.err.println("DB issue with searching articles: " + e.getMessage());
+		}
+	}
 }
